256bef949bfa51c7a5ea30dbab20fb6d19570723
RahafOthman <=>
2022-07-15 14:28:28 +0200

    vertex website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home-component' , component:HomeComponent },
+  { path: 'services-component' , component:ServicesComponent },
+  { path: 'contact-component', component:ContactComponent },
+  { path: 'about-component', component:AboutComponent },
+  { path: '' , component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
